6f7899cd879e271df632ac839533b7cf05dcd61c
abdes <abdes@users.noreply.github.com>
2022-02-26 15:46:18 +0000

    deploy: f5fb4ca8d60e3a00d66ea59358651bbbf6117596

--- master/html/_static/documentation_options.js
+++ master/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '4.1.1 (5ec9cd898e97)',
+    VERSION: '4.1.1 (f5fb4ca8d60e)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
